15a2436f69b3683af210fa8570862930a27fafce
unknown <viratprasan14@gmail.com>
2020-09-18 18:10:04 +0530

    added scroll to top

--- script/index.js
+++ script/index.js
@@ -170,3 +170,19 @@ $(()=>{
 
 
 
+// scroll on top 
+
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
+
